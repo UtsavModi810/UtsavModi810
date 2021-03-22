19d8c9257f274d0e73965bf1e6b35d335d29a060
UtsavModi810 <70595079+UtsavModi810@users.noreply.github.com>
2021-03-22 16:46:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React,{useState,useEffect} from 'react'
-import Register from "./component/Register"
-import Login  from "./component/Login"
+import React,{useState,useEffect} from 'react';
+import Register from "./component/Register";
+import Login  from "./component/Login";
+import Home from "./component/Home";
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -27,9 +28,9 @@ const App = () => {
         <Stack.Screen name="Login" component={Login}   options={{ headerShown:false
         
         }} />
-        <Stack.Screen name="Register" component={Register} options={{
-         
-        }}/>
+        <Stack.Screen name="Register" component={Register} options={{}}/>
+        <Stack.Screen name="Home" component={Home} options={{}}/>
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
